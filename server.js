baf6e16d623b6c0afcd68fa3166875b4f10feb40
Glitch (lifescope-api) <none>
2018-03-16 01:35:49 +0000

    🌎☠️ Updated with Glitch

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-import express from 'express';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import bodyParser from 'body-parser';
+import {graphqlExpress, graphiqlExpress} from 'graphql-server-express'
+import {makeExecutableSchema} from 'graphql-tools'
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
 import schema from './data/schema';
 
-const GRAPHQL_PORT = 3000;
+const URL = process.env.PROJECT_DOMAIN
+const PORT = process.env.PORT;
+const MONGO_URL = 'mongodb://localhost:27017/blog'
 
-const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+const mongoose = require('./config/mongoose');
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
-  console.log(
-    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
-  )
-);
+const db = mongoose();
+const app = express();
+
+app.use('*', cors());
+
+const userSchema = require('./graphql/index').userSchema;
+app.use('/graphql', cors(), graphqlExpress({
+  schema: userSchema,
+  rootValue: global,
+  graphiql: true
+}));
+
+// Up and Running at Port 4000
+app.listen(process.env.PORT || 4000, () => {
+  console.log('A GraphQL API running at port 4000');
+});
