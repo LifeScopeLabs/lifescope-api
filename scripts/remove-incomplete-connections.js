ca866e8630878ca6fa17d58540467cd7a26ce299
Kyle Baran <kbaran@bitscoop.com>
2018-10-31 19:08:20 -0700

    Added some maintenance scripts.

---
+++ scripts/remove-incomplete-connections.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import _ from 'lodash';
+import mongodb from 'mongodb';
+import config from 'config';
+
+import { find, readFile } from '../lib/util/fs';
+import uuid from '../lib/util/uuid';
+
+
+(function() {
+	let db;
+
+	Promise.all([
+		new Promise(function(resolve, reject) {
+			let address = config.mongodb.address;
+			let options = config.mongodb.options;
+
+			mongodb.MongoClient.connect(address, options, function(err, database) {
+				if (err) {
+					reject(err);
+				}
+				else {
+					db = database;
+					resolve();
+				}
+			});
+		}),
+	])
+	.then(async function() {
+		return db.db('live').collection('connections').remove({
+			'auth.status.complete': false
+		});
+	})
+		.then(function() {
+			console.log('Incomplete Connections removed');
+
+			db.close();
+
+			return Promise.resolve();
+		})
+		.catch(function(err) {
+			console.log(err);
+
+			if (db) {
+				db.close();
+			}
+
+			return Promise.reject(err);
+		});
+})();
---
+++ scripts/reset-running-connections.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import _ from 'lodash';
+import mongodb from 'mongodb';
+import config from 'config';
+
+import { find, readFile } from '../lib/util/fs';
+import uuid from '../lib/util/uuid';
+
+
+(function() {
+	let db;
+
+	Promise.all([
+		new Promise(function(resolve, reject) {
+			let address = config.mongodb.address;
+			let options = config.mongodb.options;
+
+			mongodb.MongoClient.connect(address, options, function(err, database) {
+				if (err) {
+					reject(err);
+				}
+				else {
+					db = database;
+					resolve();
+				}
+			});
+		}),
+	])
+	.then(async function() {
+		return db.db('live').collection('connections').updateMany({
+			status: 'running'
+		}, {
+			$set: {
+				status: 'ready'
+			}
+		});
+	})
+		.then(function() {
+			console.log('Running Connections reset');
+
+			db.close();
+
+			return Promise.resolve();
+		})
+		.catch(function(err) {
+			console.log(err);
+
+			if (db) {
+				db.close();
+			}
+
+			return Promise.reject(err);
+		});
+})();
